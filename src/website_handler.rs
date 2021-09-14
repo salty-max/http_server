b676f644bd1d1d4bd2f4eeb4a6d58fbdd29c7449
salty-max <max@jellycat.fr>
2021-09-15 00:12:19 +0200

    Implements routing

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,12 +1,22 @@
 use crate::server::Handler;
-use crate::http::request::{Request};
-use crate::http::response::Response;
-use crate::http::status_code::StatusCode;
+use crate::http::{
+	request::{Request},
+	response::Response,
+	status_code::StatusCode,
+	method::Method
+};
 
 pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
 	fn handle_request(&mut self, request: &Request) -> Response {
-		Response::new(StatusCode::Ok, Some(String::from("<h1>It works!</h1>")))
+		match request.method() {
+			Method::GET => match request.path() {
+				"/" => Response::new(StatusCode::Ok, Some(String::from("<h1>It works!</h1>"))),
+				"/hello" => Response::new(StatusCode::Ok, Some(String::from("<h1>Hello World!</h1>"))),
+				_ => Response::new(StatusCode::NotFound, None),
+			}
+			_ => Response::new(StatusCode::NotFound, None),
+		}
 	}
 }
