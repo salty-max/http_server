24d0cab32536f05dd435479736d5e744b69f8bc1
salty-max <max@jellycat.fr>
2021-09-15 00:20:50 +0200

    Handles environment variables

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -6,7 +6,15 @@ use crate::http::{
 	method::Method
 };
 
-pub struct WebsiteHandler;
+pub struct WebsiteHandler {
+	public_path: String
+}
+
+impl WebsiteHandler {
+	pub fn new(public_path: String) -> Self {
+		Self { public_path }
+	}
+}
 
 impl Handler for WebsiteHandler {
 	fn handle_request(&mut self, request: &Request) -> Response {
