19e1c2400abf62412bd7105cf1c1752dc7eb8322
salty-max <max@jellycat.fr>
2021-09-15 00:31:15 +0200

    Reads HTML from file

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use crate::server::Handler;
 use crate::http::{
 	request::{Request},
@@ -14,14 +15,19 @@ impl WebsiteHandler {
 	pub fn new(public_path: String) -> Self {
 		Self { public_path }
 	}
+
+	fn read_file(&self, file_path: &str) -> Option<String> {
+		let path = format!("{}/{}", self.public_path, file_path);
+		fs::read_to_string(path).ok()
+	}
 }
 
 impl Handler for WebsiteHandler {
 	fn handle_request(&mut self, request: &Request) -> Response {
 		match request.method() {
 			Method::GET => match request.path() {
-				"/" => Response::new(StatusCode::Ok, Some(String::from("<h1>It works!</h1>"))),
-				"/hello" => Response::new(StatusCode::Ok, Some(String::from("<h1>Hello World!</h1>"))),
+				"/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
+				"/lorem" => Response::new(StatusCode::Ok, self.read_file("lorem.html")),
 				_ => Response::new(StatusCode::NotFound, None),
 			}
 			_ => Response::new(StatusCode::NotFound, None),
